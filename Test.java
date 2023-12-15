7e7516eb5b792b11fdaa0e0e7025a06151da535c
PythonOG <114878034+PythonOG@users.noreply.github.com>
2023-12-15 11:51:08 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test{
     public static void main(String args[]){  
      System.out.println("Hello Akshay..!");
      System.out.println("Good Morning");
+     System.out.println("Welcome");
     }  
 }  
